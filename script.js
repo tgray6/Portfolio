6a576290ece37c889dd0cfcebf0cd190197d5800
Xer0 <tylergrayx@gmail.com>
2018-07-09 14:58:09 -0400

    final commit

--- script.js
+++ script.js
@@ -1,2 +1,60 @@
 "use strict";
 
+$(document).ready(function(){
+
+/*! Fades in page on load */
+$('body').hide();
+$('body').fadeIn(500);
+
+});
+
+//navbar show on burger icon click on smaller screens
+$('.hide').on('click', function() {
+	$('nav ul').toggleClass('show');
+});
+//
+
+
+$('.headerName').on('click', function(){
+	$('nav ul').removeClass('show');
+	$('.homepage').hide();
+	$('.portfolioSection').hide();
+	$('.contactSection').hide();
+	$('.bioSection').fadeIn(500);
+});
+
+
+$('.homeButton').on('click', function() {
+	$('nav ul').removeClass('show');
+	$('.bioSection').hide();
+	$('.portfolioSection').hide();
+	$('.contactSection').hide();
+	$('.homepage').fadeIn(500);
+});
+
+
+$('.bioButton').on('click', function(){
+	$('nav ul').removeClass('show');
+	$('.homepage').hide();
+	$('.portfolioSection').hide();
+	$('.contactSection').hide();
+	$('.bioSection').fadeIn(500);
+});
+
+
+$('.portfolioButton').on('click', function(){
+	$('nav ul').removeClass('show');
+	$('.homepage').hide();
+	$('.bioSection').hide();
+	$('.contactSection').hide();
+	$('.portfolioSection').fadeIn(500);
+});
+
+
+$('.contactButton').on('click', function(){
+	$('nav ul').removeClass('show');
+	$('.homepage').hide();
+	$('.bioSection').hide();
+	$('.portfolioSection').hide();
+	$('.contactSection').fadeIn(500);
+});
